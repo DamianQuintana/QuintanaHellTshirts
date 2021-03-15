a5a300687c374c7bd4a69d95f93292f84234a7a1
Damián Quintana <quintana.damian.e@gmail.com>
2021-03-15 20:51:19 -0300

    Desafio de botones

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import PropGreeting from './components/ItemListContainer';
 import Navbar  from './components/navBar';
+import ItemCount from './components/Counter'
+import {Component} from "react";
+
+
 function App() {
+  const [actualStock, setActualStock] = useState(5);
+  const buyedStock = (newStock) => 
+  setActualStock((actualStock) => actualStock-newStock);
+
   return (
     <>
     <Navbar/>
     <PropGreeting greeting ="Bienvenido a la sección del ItemListContainer" buySome="Dentro de poco vamos a tener los productos cargados"/> 
-     </>
+    <ItemCount stock={3} initial = {1} addOn = {buyedStock}/>
+    </>
   );
 }
 export default App;
